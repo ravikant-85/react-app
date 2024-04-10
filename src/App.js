db99d12c9e6b56df3747e8d21b831803fd302bea
Ravi kant <93968448+ravikant-85@users.noreply.github.com>
2024-04-10 13:11:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> HELLO RAVIKANT WELCOME TO MOBZWAY AGAIN....ravikant
+          Edit <code>src/App.js</code> HELLO RAVIKANT WELCOME TO MOBZWAY AGAIN....ravikant..
         </p>
         <a
           className="App-link"
