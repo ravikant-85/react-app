d95bcc9811716327dc5accf51a37577830d98b1b
Ravi kant <93968448+ravikant-85@users.noreply.github.com>
2024-04-10 15:21:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React app
+          Learn React app1
         </a>
       </header>
     </div>
